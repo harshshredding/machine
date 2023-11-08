c9ddaf630f8730a4f4a29f2b7f16a577dfc7c66e
harshshredding <harshshredding@gmail.com>
2023-11-08 10:10:05 -0800

    mult done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,47 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+// setup variables
+@8192
+D=A
+@numScreenByte
+M=D
+
+@currScreenByte
+M=0
+
+@drawValue
+M=0
+
+(LOOP)
+    // initialize variables
+    @currScreenByte
+    M=0
+
+    // select draw value
+    
+
+    (DRAWSCREEN)
+        // check if we are out of screen
+        @currScreenByte
+        D=M
+        @numScreenByte
+        D=M-D
+        @DONEDRAWING
+        D;JMP
+
+        @currScreenByte
+        D=M
+
+         
+        // update state 
+        @currScreenByte
+        M=M+1
+
+        @DRAWSCREEN
+        0;JMP
+    (DONEDRAWING)
+
+    @LOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -8,3 +8,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 //// Replace this comment with your code.
+
+// setup variables
+@R0
+D = M
+@a
+M = D
+
+@R1
+D = M
+@b
+M = D
+
+@acc
+M = 0
+
+(LOOP)
+    // check condition
+    @a
+    D=M
+    @DONE
+    D;JEQ
+
+    @a
+    M=M-1
+
+    @b
+    D=M
+    @acc
+    M=M+D
+
+    @LOOP
+    0;JMP
+
+(DONE)
+    // store the result
+    @acc
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0;JMP
